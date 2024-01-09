db8793739d4419dc4f6dfe69de1df4377878c424
Nurlan <nurlanabijbillaev@gmail.com>
2024-01-09 15:03:40 +0600

    Add new class

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
             System.out.println("Hello nurlan");
+            User user = new User("fsdfsdfsd");
         }
     }
 }
